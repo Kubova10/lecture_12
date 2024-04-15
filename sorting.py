a289569064e8850517b3277733bf9cd01af124dd
DominikaKubova <kubovadomi@gmail.com>
2024-04-15 08:59:41 +0200

    message

--- sorting.py
+++ sorting.py
@@ -11,9 +11,39 @@ def read_data(file_name):
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
 
+    data = {}
+    with open(file_path, "r", newline="") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        headers = next(csv_reader)
+
+        for header in headers:
+            data[header] = []
+
+        for row in csv_reader:
+            for i, value in enumerate(row):
+                data[headers[i]].append(float(value))
+
+    return data
+
+def selection_sort(list_of_nums):
+    for ind in range(len(list_of_nums)):
+        min_index = ind
+
+        for j in range(ind + 1, len(list_of_nums)):
+            # select the minimum element in every iteration
+            if list_of_nums[j] < list_of_nums[min_index]:
+                min_index = j
+        # swapping the elements to sort the array
+        (list_of_nums[ind], list_of_nums[min_index]) = (list_of_nums[min_index], list_of_nums[ind])
+    return list_of_nums
 
 def main():
-    pass
+    file_name = "numbers.csv"
+    data = read_data(file_name)
+    print(data)
+    key = data['series_1']
+    sort = selection_sort(key)
+    print(sort)
 
 
 if __name__ == '__main__':
